46d306257275723fe65abd44f48602e4138c0894
4Karolis <4karolis.t@gmail.com>
2022-06-30 19:07:00 +0300

    Created DbContext

---
+++ Db_Exam/Db_Exam/ExamDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Db_Exam.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db_Exam
+{
+    public class ExamDbContext : DbContext
+    {
+        DbSet<Department> Departments { get; set; }
+        DbSet<Lecture> Lectures { get; set; }
+        DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer($"Server=localhost;Database=ExamDb;Trusted_Connection=True;");
+        }
+    }
+}
