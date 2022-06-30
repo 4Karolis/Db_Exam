b7a2b802092aa57f39ee7e57588b512597b4266c
4Karolis <4karolis.t@gmail.com>
2022-06-30 19:31:22 +0300

    Created DbRepository

---
+++ Db_Exam/Db_Exam/DbRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db_Exam
+{
+    public class DbRepository
+    {
+        private readonly ExamDbContext _dbContext;
+        public DbRepository()
+        {
+            _dbContext = new ExamDbContext();
+        }
+    }
+}
