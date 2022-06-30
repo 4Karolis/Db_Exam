c5fba9089e644925beee344bcdcb53403e41b96d
4Karolis <4karolis.t@gmail.com>
2022-06-30 18:49:47 +0300

    Created Department Class

---
+++ Db_Exam/Db_Exam/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db_Exam.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Student> DepartmentStudents { get; set; }
+        public List<Lecture> DepartmentLectures { get; set; }
+    }
+}
