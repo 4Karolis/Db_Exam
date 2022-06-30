4c758547deebdcbea27e32297ad9240677ea2281
4Karolis <4karolis.t@gmail.com>
2022-06-30 20:10:15 +0300

    Updated Lecture_Migration

---
+++ Db_Exam/Db_Exam/Migrations/20220630170938_Updated_Lecture_Table.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Db_Exam.Migrations
+{
+    public partial class Updated_Lecture_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Lectures",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Lectures");
+        }
+    }
+}
