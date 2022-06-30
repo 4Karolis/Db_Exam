66683edf66c58eff613c8241f9be1ea5b6a7ed35
4Karolis <4karolis.t@gmail.com>
2022-06-30 18:58:38 +0300

    Created Student class

---
+++ Db_Exam/Db_Exam/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db_Exam.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Lecture> StudentLectures { get; set; }
+        public Department StudentDepartment { get; set; }
+    }
+}
