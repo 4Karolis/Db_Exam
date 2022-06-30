9d7c1247ce85141767d5d1e73782466269130fc5
4Karolis <4karolis.t@gmail.com>
2022-06-30 19:14:18 +0300

    Update Student.cs

--- Db_Exam/Db_Exam/Entities/Student.cs
+++ Db_Exam/Db_Exam/Entities/Student.cs
@@ -11,6 +11,7 @@ namespace Db_Exam.Entities
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public List<Lecture> StudentLectures { get; set; }
         public Department StudentDepartment { get; set; }
     }
