8f957b4a1e10c2edee6f8b4285b95ffd6d3b7237
4Karolis <4karolis.t@gmail.com>
2022-06-30 19:37:38 +0300

    Created BL_Servite with constructor

---
+++ Db_Exam/Db_Exam/BL_Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db_Exam
+{
+    public class BL_Service
+    {
+        private readonly DbRepository _dbRepository;
+        public BL_Service()
+        {
+            _dbRepository = new DbRepository();
+        }
+    }
+}
